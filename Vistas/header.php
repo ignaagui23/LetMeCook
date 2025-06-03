f6efa7f8edf26275ee54fb40a8cb26e1128e3a0a
Marcos22121 <marcosvillaro007@gmail.com>
2025-06-03 15:29:34 -0300

    Arreglé el ícono hamburguesa y lo reemplacé por un SVG centrado, solucioné el problema con las Cards que estaban mal en horizontal, y agregué los íconos de la slide bar (Configuración y Cerrar sesión) también en formato SVG

--- Vistas/header.php
+++ Vistas/header.php
@@ -10,7 +10,12 @@
 
   <header>
     <button class="menu-toggle" onclick="toggleSidebar()">
-      <img src="../Img/Hamburger_icon.svg.png" alt="Menú">
+      <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 100 80" fill="white">
+  <rect width="100" height="10"></rect>
+  <rect y="30" width="100" height="10"></rect>
+  <rect y="60" width="100" height="10"></rect>
+</svg>
+
     </button>
     <div class="logo">
       <img src="../Img/LogoLetMeCook.png" alt="Logo Let Me Cook">
@@ -29,11 +34,19 @@
     </div>
     <div class="bottom-buttons">
       <button>
-        <img src="gear.png" alt="Config">
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <circle cx="12" cy="12" r="3"></circle>
+  <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 1 1-4 0v-.09a1.65 1.65 0 0 0-1-1.51 1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09a1.65 1.65 0 0 0 1.51-1 1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 1 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
+</svg>
         Configuración
       </button>
       <button>
-        <img src="logout.png" alt="Cerrar sesión">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <rect x="3" y="3" width="12" height="18" rx="2" ry="2" />
+  <path d="M15 12h6" />
+  <path d="M18 9l3 3-3 3" />
+</svg>
+
         Cerrar sesión
       </button>
     </div>
